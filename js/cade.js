2aea3540fe278d428e63e88b577ca4d07634c1d9
1858221 <87901941+1858221@users.noreply.github.com>
2021-07-24 23:05:27 +0900

    Add files via upload

---
+++ js/cade.js
@@ -0,0 +1,12 @@
+function link_1(link){
+    var message = "";
+
+    if (link == 1) {
+        message = "https://vspo.jp/member/";
+    }
+    else {
+        message = "不可";
+    }
+
+    alert(message);
+}
